2cf16ab384995448345518e0b089e31b2aa9c682
solivan <solivan_lau@outlook.com>
2024-01-03 13:55:49 -0500

    added mobile menu integration w css and js. need to clean up main.js

--- js/main.js
+++ js/main.js
@@ -25,3 +25,19 @@ document.addEventListener("keydown", function (event) {
 		landAckModal.classList.add("hidden");
 	}
 });
+
+// mobile menu functionality
+let mobileMenuBtn = document.querySelector("#mobile-menu");
+let menuIconOpen = document.querySelector(".menu-icon");
+let menuIconClose = document.querySelector(".menu-exit-icon");
+
+let mobileMenuComponent = document.querySelector("#nav-links");
+
+mobileMenuBtn.addEventListener("click", () => {
+	// toggle close and open icons for menu button
+	menuIconOpen.classList.toggle("hidden");
+	menuIconClose.classList.toggle("hidden");
+
+	// toggle mobile menu component
+	mobileMenuComponent.classList.toggle("open-menu");
+});
