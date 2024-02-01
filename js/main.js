2311f1024d35edf9f9518070624b95527edafc7a
maxharrisnet <hello@maxharris.codes>
2024-02-01 08:55:30 -0800

    working on team modals

--- js/main.js
+++ js/main.js
@@ -7,64 +7,7 @@ Modals
 // HTML Reference Variables
 let openModalBtns = document.querySelectorAll(`[data-role="open-modal"]`);
 let closeModalBtns = document.querySelectorAll(`[data-role="close-modal"]`);
-
-// Team Modals Data
-
-let teamBioData = {
-	natasha: {
-		imgPath: "/assets/images/pages/team/natasha-rabsatt.png",
-		name: "Natasha Rabsatt, M.S.",
-		role: "Co-Founder",
-		bio: [
-			"Natasha is Caribbean afro-indigenous with Classic Taíno (Puerto Rico - Boríken) and African heritage.",
-			"She is passionate about creating value and social impact through mindful stewardship with innovation, technology, and creative experiences. She is an avid world traveler and has lived in many different regions of the United States and also abroad.",
-			"Professionally, Natasha is a strategic designer, tech strategist, advisor, U.S. Army veteran, and entrepreneur. She has nearly 2 decades of experience in diverse industries and sectors leading people and directing initiatives and programs centered around learning, design, innovation, business development, and technology. Over the years, she has supported a number of businesses and organizations through the design and development of inclusive learning experiences, business development, innovation strategy, creative design, and Immersive Technology - AR/VR, Emerging Technologies - NFTs, Blockchain, Web 3.0 initiatives, and more.",
-		],
-	},
-	marsha: {
-		imgPath: "/assets/images/pages/team/marsha-maxwell.png",
-		name: "Dr. Marsha Maxwell, CETL ",
-		role: "Co-founder",
-		bio: [
-			"With strategic, tactical and operational technology experience, Dr. Marsha Maxwell creates innovative environments and applications that harness the power of emerging technologies and neuroscience to enhance the educator's reach to better engage students and foster a culture of innovation in organizations.",
-			"Her areas of interest include Augmented Analytics, Immersive Experiences, Artificial Intelligence, Machine Learning, STEAM programs, and cybersecurity. Marsha has also been very active in bringing STEM/STEAM initiatives to life especially for underserved and underrepresented groups throughout their educational journey from kindergarten to university and beyond.",
-			"Marsha has been a featured speaker for various organizations' educational summits and workshops including Microsoft, Apple, and Google for her skills in education technology, innovation leadership, strategic analysis and decision-making, and equity and inclusion in technology.",
-		],
-	},
-};
-
-// Team Modal Functions
-
-// populate modal
-const populateTeamModal = profile => {
-	// deconstructing object
-	const { imgPath, name, role, bio, email, linkedin } = profile;
-
-	// Team modal elements
-	const memberImg = document.querySelector("#member-img");
-	const memberName = document.querySelector("#member-name");
-	const memberRole = document.querySelector("#member-role");
-	const memberBio = document.querySelector("#member-bio");
-
-	// picture
-	memberImg.src = imgPath;
-	memberImg.alt = `A profile picture of ${name}`;
-
-	// text
-	memberName.textContent = name;
-	memberRole.textContent = role;
-
-	// bio
-	// empty bio component
-	memberBio.innerHTML = "";
-	// populate w new bio contents
-	bio.forEach(paragraph => {
-		const pElement = document.createElement("p");
-		pElement.textContent = paragraph;
-		memberBio.appendChild(pElement);
-	});
-};
-
+console.log(openModalBtns);
 // toggle modal on and off
 const toggleModal = modal => {
 	document.body.classList.toggle("disable-scroll");
@@ -78,15 +21,18 @@ openModalBtns.forEach(button => {
 		let modalElement = document.querySelector(`#${button.dataset.type}`);
 
 		let member = button.dataset.content;
+		console.log(`Found modals: ${modalElement}`);
+
 		// opening modals based on data-content attr
 		if (member) {
 			// populate modal according to data-content
-			populateTeamModal(teamBioData[member]);
+			populateTeamModal(teamModalData[member]);
 
 			// open modal
 			toggleModal(modalElement);
 			member = "";
 		} else {
+			console.log("No member found");
 			// open modal
 			toggleModal(modalElement);
 		}
--- js/team.js
+++ js/team.js
@@ -1,3 +1,105 @@
+function generateCards(items) {
+	items.forEach(item => {
+		// Single item data
+		const data = item.acf;
+		// Container elements
+		const container = document.getElementById(`team-members-container`);
+		const card = document.createElement("li");
+		card.classList.add("member");
+
+		// Append data to the card
+		if (data.image.url) {
+			const image = document.createElement("div");
+			card.appendChild(image);
+			const img = document.createElement("img");
+			img.src = data.image.url;
+			data.image.alt ? (img.alt = data.image.alt) : (img.alt = data.name);
+			image.appendChild(img);
+		}
+
+		const text = document.createElement("div");
+		text.classList.add("text-container");
+		card.appendChild(text);
+
+		if (data.name) {
+			const name = document.createElement("h2");
+			name.textContent = data.name;
+			text.appendChild(name);
+		}
+		if (data.position) {
+			const position = document.createElement("h3");
+			position.classList.add("role");
+			position.textContent = data.position;
+			text.appendChild(position);
+		}
+		if (data.summary) {
+			const summary = document.createElement("p");
+			summary.textContent = data.summary;
+			text.appendChild(summary);
+		}
+
+		const btn = document.createElement("button");
+		btn.classList.add("btn-main", "team-modal-btn");
+		btn.setAttribute("data-role", "open-modal");
+		btn.setAttribute("data-type", "team-modal");
+		btn.setAttribute("data-content", slugify(data.name));
+		btn.textContent = "Read More";
+		text.appendChild(btn);
+
+		// Append this card to the container
+		container.appendChild(card);
+	});
+}
+
+function generateModals(items) {
+	let teamModalData = [];
+
+	items.forEach(item => {
+		const data = item.acf;
+
+		const teamMemberData = {
+			imgPath: data.image.url,
+			name: data.name,
+			role: data.position,
+			bio: data.bio,
+		};
+
+		teamModalData.push(teamMemberData);
+	});
+
+	console.log(teamModalData);
+
+	// Populate modal
+	function populateTeamModal(teamModalData) {
+		// deconstructing object
+		const { imgPath, name, role, bio, email, linkedin } = teamModalData;
+
+		// Team modal elements
+		const memberImg = document.querySelector("#modal-member-img");
+		const memberName = document.querySelector("#modal-member-name");
+		const memberRole = document.querySelector("#modal-member-role");
+		const memberBio = document.querySelector("#modal-member-bio");
+
+		// picture
+		memberImg.src = imgPath;
+		memberImg.alt = `A profile picture of ${name}`;
+
+		// text
+		memberName.textContent = name;
+		memberRole.textContent = role;
+
+		// bio
+		// empty bio component
+		memberBio.innerHTML = "";
+		// populate w new bio contents
+		bio.forEach(paragraph => {
+			const pElement = document.createElement("p");
+			pElement.textContent = paragraph;
+			memberBio.appendChild(pElement);
+		});
+	}
+}
+
 // Fetch all team members
 function getAllTeamData() {
 	const apiURL =
@@ -12,60 +114,10 @@ function getAllTeamData() {
 		.then(response => response.json())
 		.then(response => {
 			const items = response;
-
-			// Generate cards for each item
-			items.forEach((item, index) => {
-				// Single item data
-				const data = item.acf;
-				// Container elements
-				const container = document.getElementById(`team-members-container`);
-				const card = document.createElement("li");
-				card.classList.add("member");
-
-				// Append data to the card
-				if (data.image.url) {
-					const image = document.createElement("div");
-					card.appendChild(image);
-					const img = document.createElement("img");
-					img.src = data.image.url;
-					data.image.alt ? (img.alt = data.image.alt) : (img.alt = data.name);
-					image.appendChild(img);
-				}
-
-				const text = document.createElement("div");
-				text.classList.add("text-container");
-				card.appendChild(text);
-
-				if (data.name) {
-					const name = document.createElement("h2");
-					name.textContent = data.name;
-					text.appendChild(name);
-				}
-				if (data.position) {
-					const position = document.createElement("h3");
-					position.classList.add("role");
-					position.textContent = data.position;
-					text.appendChild(position);
-				}
-				if (data.summary) {
-					const summary = document.createElement("p");
-					summary.textContent = data.summary;
-					text.appendChild(summary);
-				}
-
-				const btn = document.createElement("button");
-				btn.classList.add("btn-main", "team-modal-btn");
-				btn.setAttribute("data-role", "open-modal");
-				btn.setAttribute("data-type", "team-modal");
-				btn.setAttribute("data-content", slugify(data.name));
-				btn.textContent = "Read More";
-				text.appendChild(btn);
-
-				// Append this card to the container
-				container.appendChild(card);
-			});
+			generateCards(items);
+			generateModals(items);
 		})
 		.catch(error => console.error("Error:", error));
 }
 
-getAllTeamData();
+console.log(getAllTeamData());
